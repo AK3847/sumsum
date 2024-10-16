268922e6de78434f07e984072fb7caf59c9a2cef
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-10-16 20:20:41 +0530

    base version ready for main.py

---
+++ src/sumsum/main.py
@@ -0,0 +1,99 @@
+import click
+import os
+import requests
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
+
+
+model_dir = os.path.join(os.path.expanduser("~"), ".ollama", "local_summarization")
+model_path = os.path.join(model_dir, "llama_test.gguf")
+modelfile_path = os.path.join(model_dir, "ModelFile")
+
+
+def download_model():
+    url = "https://huggingface.co/hugging-quants/Llama-3.2-1B-Instruct-Q4_K_M-GGUF/resolve/main/llama-3.2-1b-instruct-q4_k_m.gguf"
+
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get("content-length", 0))
+            progress = Progress(
+                TextColumn("[progress.description]{task.description}"),
+                BarColumn(),
+                TextColumn("[progress.percentage]{task.percentage:>3.1f}%"),
+                TimeRemainingColumn(),
+            )
+            task = progress.add_task("Downloading", total=total_size)
+
+            with progress:
+                with open(model_path, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=2048):
+                        f.write(chunk)
+                        progress.update(task, advance=len(chunk))
+        click.echo(f"Model downlaoded succesfully and saved as {model_path}")
+    except Exception as e:
+        click.echo(f"An error occured while downloading: {e}")
+
+
+def generate_model_file():
+    try:
+        with open(modelfile_path, "w") as f:
+            f.write("Test")
+        click.echo("ModelFile Generated!")
+    except Exception as e:
+        click.echo(f"An erro occurred while creating the ModelFile: {e}")
+
+
+@click.group()
+def cli():
+    """Sum-Sum : Local summarization"""
+
+
+@cli.command()
+def help():
+    """Show Options and functions"""
+
+
+@cli.command()
+def init():
+    """
+    Initialize environment
+    - Check for Ollama
+    - Download the gguf file
+    -
+    """
+
+    click.echo("Checking for Ollama......")
+    if os.system("ollama --version") == 0:
+        click.echo("Ollama is installed!")
+    else:
+        click.echo(
+            """Ollama is not installed! \nPlease install the latest version from: https://ollama.com/download"""
+        )
+        return
+
+    click.echo("Checking if model is already downloaded or not...")
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    if os.path.exists(model_path):
+        click.echo(f"Model already downloaded at {model_path}")
+    else:
+        click.echo("Downloading Model......")
+        download_model()
+
+    click.echo("Checking for Modelfile....")
+    if os.path.exists(modelfile_path):
+        click.echo(f"ModelFile already exists at {modelfile_path}.")
+        if click.confirm(
+            "Do you want to overwrite the existing ModelFile?", default=False
+        ):
+            click.echo("Overwriting ModelFile...")
+            generate_model_file()
+        else:
+            click.echo("Using the existing ModelFile.")
+    else:
+        click.echo(f"ModelFile not found!\n Generating Modelfile at {modelfile_path}.")
+        generate_model_file()
+
+
+def main():
+    cli()
